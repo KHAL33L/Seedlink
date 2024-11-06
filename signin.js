d5aa32307ed802b5fbfaedd7936f0de5f276a970
ReindorfN <144750275+ReindorfN@users.noreply.github.com>
2024-11-06 10:07:27 +0000

    modified the script

--- signin.js
+++ signin.js
@@ -3,8 +3,9 @@
 if (document.getElementById("signin_form")) {
     document.getElementById("navToSignup").addEventListener("click", function(event) {
         //Displaying signup card and hiding signin card
-        document.querySelector(".signup").style.display="flex";
         document.querySelector(".signin").style.display="none";
+        document.querySelector(".signup").style.display="flex";
+        
     });
 }
 
@@ -16,3 +17,17 @@ if (document.getElementById("signup_form")){
         document.querySelector(".signin").style.display = "flex";
     });
 }
+
+
+// document.getElementById("navToSignup").addEventListener("click", function(event) {
+//     //Displaying signup card and hiding signin card
+//     document.querySelector(".signin").style.display="none";
+//     document.querySelector(".signup").style.display="flex";
+    
+// });
+
+// document.getElementById("navToSignin").addEventListener("click", function(event) {
+//     // Hiding signup card and displaying signin card
+//     document.querySelector(".signup").style.display = "none";
+//     document.querySelector(".signin").style.display = "flex";
+// });
