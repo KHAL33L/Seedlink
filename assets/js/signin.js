587d9d930da9807d18a508221852a1b72055e84e
ReindorfN <144750275+ReindorfN@users.noreply.github.com>
2024-11-24 23:40:05 +0000

    .

--- assets/js/signin.js
+++ assets/js/signin.js
@@ -18,15 +18,15 @@ if (document.getElementById("signup_form")) {
     });
 }
 
-const email_format = /^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[a-zA-Z]{2,6}$/; //email formal
-const password_regex = /^(?=.*[A-Z])(?=(.*\d){3,})(?=.*[!@#$%^&*_()-]).{8,}$/; //password format
-
 //Signin validation function
 function signInValidation(){
     //variable declarations
     const email = document.getElementById("login_mail").value.trim();
     const password = document.getElementById("login_password").value.trim();
     const error = document.getElementById("login_error");
+
+    const email_format = /^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[a-zA-Z]{2,6}$/; //email formal
+    const password_regex = /^(?=.*[A-Z])(?=(.*\d){3,})(?=.*[!@#$%^&*_()-]).{8,}$/; //password format
 
     if (!email || !password) {
         error.innerHTML="Detected Empty fields!!";
@@ -51,14 +51,17 @@ function signUpValidation() {
     const fname = document.getElementById("fname").value.trim();
     const lname = document.getElementById("lname").value.trim();
 
-    const email = document.getElementById("signup_email").value.trim();
+    const email = document.getElementById("email").value.trim();
 
-    const password = document.getElementById("signup_password").value.trim();
+    const password = document.getElementById("password").value.trim();
     const confirm_password = document.getElementById("confirm_password").value.trim();
     const error = document.getElementById("error");
     const acceptTerms = document.querySelector('.accept input[type="checkbox"]');
     error.innerHTML = "";
 
+
+    const email_format = /^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[a-zA-Z]{2,6}$/; //email formal
+    const password_regex = /^(?=.*[A-Z])(?=(.*\d){3,})(?=.*[!@#$%^&*_()-]).{8,}$/; //password format
 
     if (!fname || !lname || !email || !password || !confirm_password) {
         error.innerHTML="Detected Empty fields!!";
