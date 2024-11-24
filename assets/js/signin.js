1ecb585ed15e8fc35cf8efd7197d53592283ff09
ReindorfN <144750275+ReindorfN@users.noreply.github.com>
2024-11-24 21:59:32 +0000

    .

--- assets/js/signin.js
+++ assets/js/signin.js
@@ -18,7 +18,8 @@ if (document.getElementById("signup_form")) {
     });
 }
 
-
+const email_format = /^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[a-zA-Z]{2,6}$/; //email formal
+const password_regex = /^(?=.*[A-Z])(?=(.*\d){3,})(?=.*[!@#$%^&*_()-]).{8,}$/; //password format
 
 //Signin validation function
 function signInValidation(){
@@ -41,4 +42,50 @@ function signInValidation(){
     error.style.color="green";
     error.innerHTML="Log In Successful!!";
     return true;
+}
+
+
+//Signup validation function
+function signUpValidation() {
+    // Variable declarations
+    const fname = document.getElementById("fname").value.trim();
+    const lname = document.getElementById("lname").value.trim();
+
+    const email = document.getElementById("email").value.trim();
+
+    const password = document.getElementById("password").value.trim();
+    const confirm_password = document.getElementById("confirm_password").value.trim();
+    const error = document.getElementById("error");
+    const acceptTerms = document.querySelector('.accept input[type="checkbox"]');
+    error.innerHTML = "";
+
+
+    if (!fname || !lname || !email || !password || !confirm_password) {
+        error.innerHTML="Detected Empty fields!!";
+        console.log("False recorder");
+        return false;  
+    } if (!email_format.test(email)) {
+        error.innerHTML="The email format is wrong!!";
+        return false;
+    } 
+   if (confirm_password !== password) {
+        error.innerHTML = "Passwords do not match!!";
+        return false;
+    } if (!acceptTerms.checked) {
+        error.innerHTML = "You must accept the Terms & Conditions!";
+        return false;
+    }
+    if (!password_regex.test(password)) {
+        console.log(password)
+        console.log("Password validation failed!!");
+        error.innerHTML = "Password must contain at least 8 characters, one uppercase letter, three digits, and one special character!";
+        return false;
+    } 
+
+    //success
+    error.style.color = "green";
+    error.innerHTML="Signup Successful!!";
+    console.log("Validation passed: signup successful")
+    return true;
+
 }
