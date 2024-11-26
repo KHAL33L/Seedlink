218b3992b20155e2e7baa73c50e424468599e329
KHAL33L <dasukikhaleel@gmail.com>
2024-11-26 15:31:58 +0000

    linked the footer "contact us" to the about page

--- view/profile.php
+++ view/profile.php
@@ -120,7 +120,7 @@ $email = $_SESSION["email"];
             <ul class="footer-links">
                 <li><a href="#">Privacy Policy</a></li>
                 <li><a href="#">Terms of Service</a></li>
-                <li><a href="#">Contact Us</a></li>
+                <li><a href="about.php">Contact Us</a></li>
             </ul>
         </div>
     </footer>
